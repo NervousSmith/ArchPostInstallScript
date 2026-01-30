7c67fc041aa2cbb76dc4d4adb232b8e4a10223d1
Paweł Kowalski <48060180+NervousSmith@users.noreply.github.com>
2026-01-30 16:28:23 +0100

    change libreoffice to fresh

--- post_install_script.sh
+++ post_install_script.sh
@@ -25,5 +25,5 @@ sudo pacman -Syu
 
 echo "Installing packages"
 
-sudo pacman -S solaar libreoffice-still okular 7zip discord firefox flatpak base-devel neovim wine gamemode gamescope steam partitionmanager mission-center kate libreoffice-still-pl powerdevil adobe-source-sans-pro-fonts ttf-dejavu ttf-opensans noto-fonts freetype2 terminus-font ttf-bitstream-vera ttf-dejavu ttf-droid ttf-fira-mono ttf-fira-sans ttf-freefont ttf-inconsolata ttf-liberation libertinus-font git --noconfirm  
+sudo pacman -S solaar libreoffice-fresh okular 7zip discord firefox flatpak base-devel neovim wine gamemode gamescope steam partitionmanager mission-center kate libreoffice-fresh-pl powerdevil adobe-source-sans-pro-fonts ttf-dejavu ttf-opensans noto-fonts freetype2 terminus-font ttf-bitstream-vera ttf-dejavu ttf-droid ttf-fira-mono ttf-fira-sans ttf-freefont ttf-inconsolata ttf-liberation libertinus-font git --noconfirm  
 
