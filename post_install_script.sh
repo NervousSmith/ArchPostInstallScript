788d801a2f8256c8006a2af199a24951938833b6
Paweł Kowalski <48060180+NervousSmith@users.noreply.github.com>
2026-02-09 14:05:59 +0100

    Update post_install_script.sh

--- post_install_script.sh
+++ post_install_script.sh
@@ -25,5 +25,4 @@ sudo pacman -Syu
 
 echo "Installing packages"
 
-sudo pacman -S solaar libreoffice-fresh okular 7zip discord flatpak base-devel neovim wine gamemode gamescope steam partitionmanager mission-center kate libreoffice-fresh-pl powerdevil adobe-source-sans-pro-fonts ttf-dejavu ttf-opensans noto-fonts freetype2 terminus-font ttf-bitstream-vera ttf-dejavu ttf-droid ttf-fira-mono ttf-fira-sans ttf-freefont ttf-inconsolata ttf-liberation libertinus-font git --noconfirm  
-
+sudo pacman -S solaar libreoffice-fresh okular 7zip discord flatpak base-devel neovim wine gamemode gamescope steam partitionmanager mission-center kate libreoffice-fresh-pl powerdevil git --noconfirm  
